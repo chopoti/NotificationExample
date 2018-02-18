0cf7b6605cdd96ebe266bc050d7f3897871c1987
VickY <kmvignesh94@gmail.com>
2018-02-18 20:59:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vicky/notificationexample/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.vicky.notificationexample
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RemoteViews
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private val channelId = "com.example.vicky.notificationexample"
+    private val description = "Test notification"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        btn_notify.setOnClickListener {
+
+            val intent = Intent(this,MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val contentView = RemoteViews(packageName,R.layout.notification_layout)
+            contentView.setTextViewText(R.id.tv_title,"CodeAndroid")
+            contentView.setTextViewText(R.id.tv_content,"Text notification")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(channelId,description,NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this,channelId)
+                        .setContent(contentView)
+                        .setSmallIcon(R.drawable.ic_launcher_round)
+                        .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher))
+                        .setContentIntent(pendingIntent)
+            }else{
+
+                builder = Notification.Builder(this)
+                        .setContent(contentView)
+                        .setSmallIcon(R.drawable.ic_launcher_round)
+                        .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher))
+                        .setContentIntent(pendingIntent)
+            }
+            notificationManager.notify(1234,builder.build())
+        }
+
+    }
+}
